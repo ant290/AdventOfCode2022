9528eb1b2480153b4954520471b38d536c2eb217
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2022-12-01 10:45:17 +0000

    Day1

---
+++ Day1/Day1/BackpackProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class BackpackProcessor
+    {
+        List<Backpack>? _backpacks;
+
+        public void ProcessBackpackData(IEnumerable<string> data)
+        {
+            //clear stored backpacks
+            _backpacks = new List<Backpack>();
+
+            //loop through data and generate new backpacks
+            Backpack? currentPack = null;
+            foreach (string item in data)
+            {
+                if (string.IsNullOrWhiteSpace(item))
+                {
+                    if(currentPack != null) _backpacks.Add(currentPack);
+                    currentPack = null;
+                    continue;
+                }
+
+                currentPack ??= new Backpack();
+                currentPack.AddCaloricItem(int.Parse(item));
+            }
+            if (currentPack != null) _backpacks.Add(currentPack);
+        }
+
+        public Backpack? GetFattestPack()
+        {
+            return _backpacks?.MaxBy(x => x.TotalCalories);
+        }
+
+        public IEnumerable<Backpack>? GetPacksBySize()
+        {
+            return _backpacks?.OrderByDescending(x => x.TotalCalories);
+        }
+    }
+
+    public class Backpack
+    {
+        public List<int> MealCalories { get; set; }
+        public int TotalCalories => MealCalories.Sum();
+
+        public Backpack()
+        {
+            MealCalories = new List<int>();
+        }
+
+        public void AddCaloricItem(int calories)
+        {
+            MealCalories.Add(calories);
+        }
+    }
+}
---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day1;
+
+Console.WriteLine("Calorie Counter!");
+
+var lines = File.ReadLines(@"data.txt");
+
+var processor = new BackpackProcessor();
+processor.ProcessBackpackData(lines);
+Console.WriteLine(processor.GetFattestPack()?.TotalCalories);
+
+var packsList = processor.GetPacksBySize();
+if (packsList != null)
+{
+    var total = 0;
+    foreach (Backpack backpack in packsList.Take(3))
+    {
+        total += backpack.TotalCalories;
+    }
+    Console.WriteLine(total);
+}
