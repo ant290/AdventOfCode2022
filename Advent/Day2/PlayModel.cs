0908ff8d80bdd9ec0852bc7c2845a5bb02df6847
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2022-12-02 17:23:06 +0000

    Day2 pt2

--- Advent/Day2/PlayModel.cs
+++ Advent/Day2/PlayModel.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day2
+﻿namespace Day2
 {
     public class PlayModel
     {
-        public int WinVal = 6;
-        public int DrawVal = 3;
-        public int LoseVal = 0;
+        public const int WinVal = 6;
+        public const int DrawVal = 3;
+        public const int LoseVal = 0;
+
+        public const char LoseChar = 'X';
+        public const char DrawChar = 'Y';
+        public const char WinChar = 'Z';
+
+        public const char RockChar = 'A';
+        public const char PaperChar = 'B';
+        public const char ScissorsChar = 'C';
 
         public Dictionary<string, int> HandPointsDict = new Dictionary<string, int>()
         {
@@ -26,50 +28,95 @@ namespace Day2
         public char TheirHand;
         public char MyHand;
 
-        public PlayModel(char theirHand, char myHand)
+        public PlayModel(char theirHand, char requiredResult)
         {
             TheirHand = theirHand;
-            MyHand = myHand;
+            MyHand = HandToPlay(requiredResult);
 
             MyScore = CalculateMyScore();
+        }
+
+        private char HandToPlay(char requiredResult)
+        {
+            switch (TheirHand)
+            {
+                case RockChar:
+                    switch (requiredResult)
+                    {
+                        case LoseChar:
+                            return ScissorsChar;
+                        case DrawChar:
+                            return RockChar;
+                        case WinChar:
+                            return PaperChar;
+                        default:
+                            return '0';
+                    }
+                case PaperChar:
+                    switch (requiredResult)
+                    {
+                        case LoseChar:
+                            return RockChar;
+                        case DrawChar:
+                            return PaperChar;
+                        case WinChar:
+                            return ScissorsChar;
+                        default:
+                            return '0';
+                    }
+                case ScissorsChar:
+                    switch (requiredResult)
+                    {
+                        case LoseChar:
+                            return PaperChar;
+                        case DrawChar:
+                            return ScissorsChar;
+                        case WinChar:
+                            return RockChar;
+                        default:
+                            return '0';
+                    }
+                default:
+                    return '0';
+            }
         }
 
         private int CalculateMyScore()
         {
             switch (TheirHand)
             {
-                case 'A':
+                case RockChar:
                     switch (MyHand)
                     {
-                        case 'X':
+                        case RockChar:
                             return HandPointsDict["X"] + DrawVal;
-                        case 'Y':
+                        case PaperChar:
                             return HandPointsDict["Y"] + WinVal;
-                        case 'Z':
+                        case ScissorsChar:
                             return HandPointsDict["Z"];
                         default:
                             return 0;
                     }
-                case 'B':
+                case PaperChar:
                     switch (MyHand)
                     {
-                        case 'X':
+                        case RockChar:
                             return HandPointsDict["X"];
-                        case 'Y':
+                        case PaperChar:
                             return HandPointsDict["Y"] + DrawVal;
-                        case 'Z':
+                        case ScissorsChar:
                             return HandPointsDict["Z"] + WinVal;
                         default:
                             return 0;
                     }
-                case 'C':
+                case ScissorsChar:
                     switch (MyHand)
                     {
-                        case 'X':
+                        case RockChar:
                             return HandPointsDict["X"] + WinVal;
-                        case 'Y':
+                        case PaperChar:
                             return HandPointsDict["Y"];
-                        case 'Z':
+                        case ScissorsChar:
                             return HandPointsDict["Z"] + DrawVal;
                         default:
                             return 0;
