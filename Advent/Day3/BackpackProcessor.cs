619ff327dd5bb2679c5e970ae39b219ca2ba4217
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2022-12-07 14:58:24 +0000

    Day 3 pt1

---
+++ Advent/Day3/BackpackProcessor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class BackpackProcessor
+    {
+        private List<Backpack> _backpacks;
+
+        public BackpackProcessor()
+        {
+            _backpacks = new List<Backpack>();
+        }
+        internal void ProcessBackpackData(IEnumerable<string> data)
+        {
+            foreach (string s in data)
+            {
+                _backpacks.Add(new Backpack(s));
+            }
+        }
+
+        internal int GetValueOfMissplacedItems()
+        {
+            var res = 0;
+            foreach (var item in _backpacks)
+            {
+                res += item.CharValue;
+            }
+
+            return res;
+        }
+    }
+
+    internal class Backpack
+    {
+        private List<string> _compartments;
+        internal char? MatchingCharacter => MatchingChar();
+        internal int CharValue => MatchingChar() != null ? GetCharValue((char)MatchingChar()) : 0;
+        public Backpack(string items)
+        {
+            _compartments = new List<string>();
+            var len = items.Length/2;
+            _compartments.Add(items.Remove(len));
+            _compartments.Add(items.Substring(len));
+        }
+
+        private char? MatchingChar()
+        {
+            if (_compartments.Count == 2)
+            {
+                var res = _compartments[0].Intersect(_compartments[1]);
+                return res.FirstOrDefault();
+            }
+            
+            return null;
+        }
+
+        private int GetCharValue(char c)
+        {
+            bool isUpper = char.IsUpper(c);
+            int index = char.ToUpper(c) - ('A' - 1);
+            return isUpper ? index + 26 : index;
+        }
+    }
+}
---
+++ Advent/Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day3;
+
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadLines(@"data.txt");
+
+var processor = new BackpackProcessor();
+processor.ProcessBackpackData(lines);
+
+Console.WriteLine(processor.GetValueOfMissplacedItems());
